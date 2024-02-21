61e41c606ae7ce0644eb201a9b9b146b49fcd51f
Gabrielle Browne <gabrielle.browne@mail.utoronto.ca>
2024-02-21 12:53:43 -0500

    before_solver python file created to get approximate time of processes before while loop

---
+++ before_solver.py
@@ -0,0 +1,45 @@
+# import polyscope as ps
+import numpy as np
+import igl
+import scipy as sp
+from datetime import datetime
+import time
+import psutil
+# import matplotlib.pyplot as plt
+
+percpu = psutil.cpu_percent(percpu=True)
+cpu_count = psutil.cpu_count()
+v, _, _, f, _, _ = igl.read_obj("octopus.mesh__sf.obj")
+# ps.init()
+# ps.register_surface_mesh("octopus", v, f)
+# ps.show()
+
+L = igl.cotmatrix(v, f)
+# print(L)
+
+
+# plt.spy(L)
+eps = 1e-12
+i1 = np.where(v[:, 1] == v[:, 1].max())[0] # top of octopus, indices known
+i2 = np.where(v[:, 0] == v[:, 0].min())[0]
+
+k = np.concatenate([i1, i2])
+
+n = v.shape[0]
+d = np.setdiff1d(np.arange(n), k)
+
+Ldd = L[d, :][:, d]
+Ldk = L[d, :][:, k]
+
+
+# assemble uk=2 vector (u[k])
+u_k = np.array([-5, 20])
+# solve Ldd * udd = uk
+
+rhs = -Ldk @ u_k
+
+counter = 0
+title = datetime.today()
+time_file = open(f"before_solver{title: %B%d%Y}.txt", "a")
+start_time = datetime.now()
+t1 = time.time()

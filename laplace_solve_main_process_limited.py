1e3cdaff4a700fd6f146f7e2b1513098c3e14343
Gabrielle Browne <gabrielle.browne@mail.utoronto.ca>
2024-05-15 16:36:15 -0400

    tolerance 10^-5 data redone

--- laplace_solve_main_process_limited.py
+++ laplace_solve_main_process_limited.py
@@ -1,11 +1,11 @@
 import os
 
 num_threads = '1'
-# os.environ["MKL_NUM_THREADS"] = num_threads
-# os.environ["NUMEXPR_NUM_THREADS"] = num_threads
-# os.environ["OMP_NUM_THREADS"] = num_threads
-# os.environ['OPENBLAS_NUM_THREADS'] = num_threads
-# os.environ['VECLIB_MAXIMUM_THREADS'] = num_threads
+os.environ["MKL_NUM_THREADS"] = num_threads
+os.environ["NUMEXPR_NUM_THREADS"] = num_threads
+os.environ["OMP_NUM_THREADS"] = num_threads
+os.environ['OPENBLAS_NUM_THREADS'] = num_threads
+os.environ['VECLIB_MAXIMUM_THREADS'] = num_threads
 
 import numpy as np
 import igl
